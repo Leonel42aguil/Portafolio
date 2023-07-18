3cd518ff8635dc6af4d5c8c49f63fea4851780fb
Leonel aguilera <Leonelcarp142nuevo@gmail.com>
2023-07-17 21:02:09 -0300

    Ya casi terminado

--- js/script.js
+++ js/script.js
@@ -4,3 +4,18 @@ window.onscroll = () => {
 
   header.classList.toggle('sticky', window.scrolly > 100);
 };
+ 
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 1,
+  spaceBetween: 50,
+  loop: true,
+  grabCursor: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev"
+  }
+})

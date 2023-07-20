4f39be6147b4c5abfc9ef6ba7a494e8f0c680033
Leonel aguilera <Leonelcarp142nuevo@gmail.com>
2023-07-20 15:39:49 -0300

    subiendo

--- js/script.js
+++ js/script.js
@@ -1,9 +1,36 @@
+let menuIcon = document.querySelector('#menu-icon')
+let navBar = document.querySelector('.navbar')
+
+menuIcon.onclick = () => {
+  menuIcon.classList.toggle('bx-x')
+  navBar.classList.toggle('active')
+}
+
+let sections = document.querySelectorAll('section')
+let navLinks = document.querySelectorAll('header nav a')
 
 window.onscroll = () => {
+  sections.forEach(sec => {
+    let top = window.scrollY;
+    let offser = sec.offsetTop - 150;
+    let height = sec.offsetHeight
+    let id = sec.getAttribute('id')
+
+    if (top >= offset && top < offset + height) {
+      navLinks.forEach(links => {
+        links.classList.remove('active')
+        document.querySelector('header nav a[href*=' + id + ']').classList.add('active')
+      })
+    }
+  })
+
   let header = document.querySelector('.header');
 
   header.classList.toggle('sticky', window.scrolly > 100);
 };
+
+menuIcon.classList.remove('bx-x')
+navBar.classList.remove('active')
  
 var swiper = new Swiper(".mySwiper", {
   slidesPerView: 1,
@@ -19,3 +46,14 @@ var swiper = new Swiper(".mySwiper", {
     prevEl: ".swiper-button-prev"
   }
 })
+
+let darkModeIcon = document.querySelector('#darkMode-icon')
+
+darkModeIcon.onclick = () => {
+   darkModeIcon.classList.toggle('bx-sun')
+   document.body.classList.toggle('dark-mode')
+}
+
+
+
+
